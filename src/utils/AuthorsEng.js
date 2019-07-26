f395c6ae1402194386e531bbc10325590e1ea690
Sasha <44711578+Sannnao@users.noreply.github.com>
2019-07-26 16:16:21 +0300

    fix: copy and multiply author

--- src/utils/AuthorsEng.js
+++ src/utils/AuthorsEng.js
@@ -36,4 +36,226 @@ export default [
       }
     ],
   },
-]
+  {
+    name: 'vasia pupkin',
+    photo: 'src',
+    yearsOfLife: '',
+    biography: [
+      {
+        date: '1970',
+        content: 'Я родился в москве в 70х, на краю города...'
+      },
+    ],
+    listOfWorks: [
+      {
+        date: 'date',
+        work: 'work',
+      },
+    ],
+    gallery: {
+      authorsPicture: 'src',
+      worksPictures: [
+        {
+          name: 'name',
+          img: 'src',
+        },
+      ],
+    },
+    youtubeVideos: {
+      aboutAuthor: 'src',
+      aboutWorks: 'src',
+      biographyVideo: 'src',
+    },
+    placesOfActivity: [
+      {
+        activity: 'name',
+        mapSrc: 'google, please',
+      }
+    ],
+  },
+  {
+    name: 'vasia pupkin',
+    photo: 'src',
+    yearsOfLife: '',
+    biography: [
+      {
+        date: '1970',
+        content: 'Я родился в москве в 70х, на краю города...'
+      },
+    ],
+    listOfWorks: [
+      {
+        date: 'date',
+        work: 'work',
+      },
+    ],
+    gallery: {
+      authorsPicture: 'src',
+      worksPictures: [
+        {
+          name: 'name',
+          img: 'src',
+        },
+      ],
+    },
+    youtubeVideos: {
+      aboutAuthor: 'src',
+      aboutWorks: 'src',
+      biographyVideo: 'src',
+    },
+    placesOfActivity: [
+      {
+        activity: 'name',
+        mapSrc: 'google, please',
+      }
+    ],
+  },
+  {
+    name: 'vasia pupkin',
+    photo: 'src',
+    yearsOfLife: '',
+    biography: [
+      {
+        date: '1970',
+        content: 'Я родился в москве в 70х, на краю города...'
+      },
+    ],
+    listOfWorks: [
+      {
+        date: 'date',
+        work: 'work',
+      },
+    ],
+    gallery: {
+      authorsPicture: 'src',
+      worksPictures: [
+        {
+          name: 'name',
+          img: 'src',
+        },
+      ],
+    },
+    youtubeVideos: {
+      aboutAuthor: 'src',
+      aboutWorks: 'src',
+      biographyVideo: 'src',
+    },
+    placesOfActivity: [
+      {
+        activity: 'name',
+        mapSrc: 'google, please',
+      }
+    ],
+  },
+  {
+    name: 'vasia pupkin',
+    photo: 'src',
+    yearsOfLife: '',
+    biography: [
+      {
+        date: '1970',
+        content: 'Я родился в москве в 70х, на краю города...'
+      },
+    ],
+    listOfWorks: [
+      {
+        date: 'date',
+        work: 'work',
+      },
+    ],
+    gallery: {
+      authorsPicture: 'src',
+      worksPictures: [
+        {
+          name: 'name',
+          img: 'src',
+        },
+      ],
+    },
+    youtubeVideos: {
+      aboutAuthor: 'src',
+      aboutWorks: 'src',
+      biographyVideo: 'src',
+    },
+    placesOfActivity: [
+      {
+        activity: 'name',
+        mapSrc: 'google, please',
+      }
+    ],
+  },
+  {
+    name: 'vasia pupkin',
+    photo: 'src',
+    yearsOfLife: '',
+    biography: [
+      {
+        date: '1970',
+        content: 'Я родился в москве в 70х, на краю города...'
+      },
+    ],
+    listOfWorks: [
+      {
+        date: 'date',
+        work: 'work',
+      },
+    ],
+    gallery: {
+      authorsPicture: 'src',
+      worksPictures: [
+        {
+          name: 'name',
+          img: 'src',
+        },
+      ],
+    },
+    youtubeVideos: {
+      aboutAuthor: 'src',
+      aboutWorks: 'src',
+      biographyVideo: 'src',
+    },
+    placesOfActivity: [
+      {
+        activity: 'name',
+        mapSrc: 'google, please',
+      }
+    ],
+  },
+  {
+    name: 'vasia pupkin',
+    photo: 'src',
+    yearsOfLife: '',
+    biography: [
+      {
+        date: '1970',
+        content: 'Я родился в москве в 70х, на краю города...'
+      },
+    ],
+    listOfWorks: [
+      {
+        date: 'date',
+        work: 'work',
+      },
+    ],
+    gallery: {
+      authorsPicture: 'src',
+      worksPictures: [
+        {
+          name: 'name',
+          img: 'src',
+        },
+      ],
+    },
+    youtubeVideos: {
+      aboutAuthor: 'src',
+      aboutWorks: 'src',
+      biographyVideo: 'src',
+    },
+    placesOfActivity: [
+      {
+        activity: 'name',
+        mapSrc: 'google, please',
+      }
+    ],
+  },
+];
