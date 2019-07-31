6a6b5c4ff375b294ebe0bdf76de450265a11a2b2
Alexander <thsssanya228@gmail.com>
2019-07-31 20:47:31 +0300

    feat: add getting info from props to PoetAllInfo

--- src/components/poetAllInfo/PoetAllInfo.jsx
+++ src/components/poetAllInfo/PoetAllInfo.jsx
@@ -25,94 +25,21 @@ const useStyles = makeStyles(theme => ({
 const PoetAllInfo = props => {
   const classes = useStyles();
 
-  const poetInfo = {
-    name: 'Alexander',
-    surname: 'Kovalenko',
-    yearsOfLife: 'July 15, 1876 – November 5, 1916',
-    picture: 'https://avatars0.githubusercontent.com/u/40535982?v=4',
-  };
-
-  const poetBio = [
-    {
-      date: '1902-1904',
-      content:
-        'After moving to Pashkevich Petersburg, in 1902, she graduated externally from the Gymnasium Alexandria for girls and joined a school for physical education teachers, Lieshafta AF',
-    },
-    {
-      date: '1904',
-      content:
-        "Pashkievich was one of the founders of the Belarusian Socialist Assembly. In 1904, she gave up teaching and returned to Vilnius. She organized workers' groups, wrote and promoted anti-government proclamations, and took part in debates and political meetings.",
-    },
-    {
-      date: '1908-1909',
-      content:
-        "In 1908–1909 she lived in Krakow and studied at the Jagiellonian University's Faculty of humanities..",
-    },
-    {
-      date: '1911',
-      content:
-        'In 1911, she married Steponas Kairys, a Lithuanian engineer and social democracy activist. In the same year, she returned to Belarus and joined national educational activities.',
-    },
-    {
-      date: '1914-1915',
-      content:
-        'During World War I, Alaiza Pashkievich worked as a Sister of Charity in a military hospital in Vilnius.',
-    },
-    {
-      date: '1916',
-      content:
-        'At the beginning of 1916, she traveled to her parents and helped villagers sick with typhoid. She also fell ill with typhus and died 5 February 1916.',
-    },
-  ];
-
-  const listOfWorks = [
-    {
-      date: '1904',
-      work: "Man hasn't changed",
-    },
-    {
-      date: '1905',
-      work: 'Faith of a Belarusian',
-    },
-    {
-      date: '1905',
-      work: "On the New Year's Eve",
-    },
-    {
-      date: '1906',
-      work: 'Belarusian violin',
-    },
-    {
-      date: '1906',
-      work: 'Cross to the freedom',
-    },
-    {
-      date: '1906',
-      work: 'With the banner',
-    },
-  ];
-
-  const interFace = {
-    title: 'Works',
-    firstColumnName: 'Date',
-    secondColumnName: 'Name',
-  };
-
-  const videoId = 'GlYYLSkqG90';
-
-  const placesOfActivity = [
-    {
-      activity: 'Памятник Алаизе Пашкевич',
-      mapLink: { lat: 53.606939, lng: 24.741627 },
-    },
-  ];
+  const {
+    poetInfo,
+    poetBio,
+    listOfWorks,
+    poetWorksInterface,
+    videoId,
+    placesOfActivity,
+  } = props;
 
   return (
     <Grid container className={classes.poetAllInfoContainer} justify="center">
       <Paper className={classes.poetAllInfoRoot}>
         <PoetTitle poetInfo={poetInfo} />
         <PoetBiograpyhy poetBio={poetBio} title="Biography" />
-        <PoetWorks listOfWorks={listOfWorks} interface={interFace} />
+        <PoetWorks listOfWorks={listOfWorks} interface={poetWorksInterface} />
         <PoetVideo videoId={videoId} />
         <PoetMap
           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
